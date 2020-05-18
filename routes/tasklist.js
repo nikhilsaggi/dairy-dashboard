5e9945a1451edc066ff218953b1de0db03997b29
Nikhil Saggi <ns739@cornell.edu>
2020-05-18 12:57:44 -0400

    adding (broken) get request

--- routes/tasklist.js
+++ routes/tasklist.js
@@ -14,48 +14,156 @@ const TaskDao = require("../models/TaskDao");
    }
 
    async showTasks(req, res) {
-     const querySpec = {
-       query: "SELECT TOP 14 c.DIM, c.Protein, c.Yieldgr, c.Fat, c.Blood, c.datets  FROM c WHERE c.AnimalID = 26 ORDER BY c.datets DESC"
-     };
+    var ARRAYS = [26, 86, 2714];
+    var dataList = new Array();
 
-     const items = await this.taskDao.find(querySpec);
-     items.reverse();
+    
 
-     //Create the arrays for the charts 
-     const j = {};
-     items.forEach(row => {
+    // ARRAYS.forEach(async (id) => {
+
+      const querySpec = {
+        query: "SELECT TOP 14 c.DIM, c.Protein, c.Yieldgr, c.Fat, c.Blood, c.datets  FROM c WHERE c.AnimalID = 26 ORDER BY c.datets DESC"
+        // query: "SELECT c.DIM, c.Protein, c.Yieldgr, c.Fat, c.Blood, c.datets  FROM c WHERE c.AnimalID = ${id} ORDER BY c.datets DESC"
+      };
+
+      const items = await this.taskDao.find(querySpec);
+      items.reverse();
+
+      //Create the arrays for the charts 
+      const j = {};
+      items.forEach(row => {
+        // console.log(row)
+
+        // Create a new JavaScript Date object based on the timestamp
+        // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+        var date = new Date(row.datets * 1000);
+        row.dates = date;
+
+        Object.keys(row).forEach(key => {
+
+          if(this.isValid(row[key]) && key !== 'datets') {
+
+            if(j[key] == null){
+              j[key] = {};
+              var x = []
+              x.push(row[key])
+              j[key][key] = x
+              j[key].time = [row.datets]
+            }else{
+              j[key][key].push(row[key])
+              j[key].time.push(row.datets)
+            }  
+          }
+        })
+      });
+
+      dataList.push(j);
+    // });
+
+      
+
+
+    const querySpec2 = {
+      query: "SELECT TOP 14 c.DIM, c.Protein, c.Yieldgr, c.Fat, c.Blood, c.datets  FROM c WHERE c.AnimalID = 26 ORDER BY c.datets DESC"
+      // query: "SELECT c.DIM, c.Protein, c.Yieldgr, c.Fat, c.Blood, c.datets  FROM c WHERE c.AnimalID = ${id} ORDER BY c.datets DESC"
+    };
+
+    const items2 = await this.taskDao.find(querySpec2);
+    items2.reverse();
+
+    //Create the arrays for the charts 
+    const j2 = {};
+    items2.forEach(row => {
       // console.log(row)
 
       // Create a new JavaScript Date object based on the timestamp
       // multiplied by 1000 so that the argument is in milliseconds, not seconds.
       var date = new Date(row.datets * 1000);
-      row.datets = date.toLocaleDateString();
+      row.dates = date;
 
       Object.keys(row).forEach(key => {
 
         if(this.isValid(row[key]) && key !== 'datets') {
 
-          if(j[key] == null){
-            j[key] = {};
+          if(j2[key] == null){
+            j2[key] = {};
             var x = []
             x.push(row[key])
-            j[key][key] = x
-            j[key].time = [row.datets]
+            j2[key] = x
+            j2[key].time = [row.datets]
           }else{
-            j[key][key].push(row[key])
-            j[key].time.push(row.datets)
+            j2[key].push(row[key])
+            j2[key].time.push(row.datets)
           }  
         }
       })
-     });
+    });
+
+    dataList.push(j2);
 
 
-    //  console.log(j);
+
+
+
+
+
+    const querySpec3 = {
+      query: "SELECT TOP 14 c.DIM, c.Protein, c.Yieldgr, c.Fat, c.Blood, c.datets  FROM c WHERE c.AnimalID = 26 ORDER BY c.datets DESC"
+      // query: "SELECT c.DIM, c.Protein, c.Yieldgr, c.Fat, c.Blood, c.datets  FROM c WHERE c.AnimalID = ${id} ORDER BY c.datets DESC"
+    };
+
+    const items3 = await this.taskDao.find(querySpec3);
+    items3.reverse();
+
+    //Create the arrays for the charts 
+    const j3 = {};
+    items3.forEach(row => {
+      // console.log(row)
+
+      // Create a new JavaScript Date object based on the timestamp
+      // multiplied by 1000 so that the argument is in milliseconds, not seconds.
+      var date = new Date(row.datets * 1000);
+      row.dates = date;
+
+      Object.keys(row).forEach(key => {
+
+        if(this.isValid(row[key]) && key !== 'datets') {
+
+          if(j3[key] == null){
+            j3[key] = {};
+            var x = []
+            x.push(row[key])
+            j3[key][key] = x
+            j3[key].time = [row.datets]
+          }else{
+            j3[key][key].push(row[key])
+            j3[key].time.push(row.datets)
+          }  
+        }
+      })
+    });
+
+    dataList.push(j3);
+
+
+
+
+
+
+
+
+
+
+
+    console.log("FINAL");
+    console.log(dataList);
+    // Promise.all(dataList).then(
      res.render("index", {
        title: "Cow Data",
-       tasks: JSON.stringify(j),
+       tasks: JSON.stringify(dataList),
 
      });
+    // );
    }
 
    async addTask(req, res) {
